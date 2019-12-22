41888d7bf675cd4a37ee8309dc3037ff381aa401
Sebastian Roelfes <sr751633@fh-muenster.de>
2019-12-22 23:58:36 +0100

    Added Selenium Stuff for fun

---
+++ main.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def main():
+    print('Hello World, lets start Coding')
+    if __name__ == '__main__':
+        print('Starting with Selenium')
+
+        driver = webdriver.Chrome()
+        driver.get("http://www.google.de")
+        assert "Google" in driver.title
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("Giraffe")
+        elem.send_keys(Keys.ENTER)
+        elem = driver.find_element_by_xpath("//*")
+        source_code= elem.get_attribute('outerHTML')
+        print(source_code)
+        driver.close()
+
+main()
+
+
