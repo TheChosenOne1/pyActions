e3e3a5ec8bf555ba672df0626e6a4d04c3ca313d
Sebastian Roelfes <sr751633@fh-muenster.de>
2019-12-23 12:51:34 +0100

    Habe print Jo Tobi Kommentar hinzugefuegt

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ def main():
     print('Hello World, lets start Coding')
     if __name__ == '__main__':
         print('Starting with Selenium')
-
-        driver = webdriver.Chrome()
-        driver.get("http://www.google.de")
-        assert "Google" in driver.title
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("Giraffe")
-        elem.send_keys(Keys.ENTER)
-        elem = driver.find_element_by_xpath("//*")
-        source_code= elem.get_attribute('outerHTML')
-        print(source_code)
-        driver.close()
+        print('Jo Tobi geht voll ab')
+        # driver = webdriver.Chrome()
+        # driver.get("http://www.google.de")
+        # assert "Google" in driver.title
+        # elem = driver.find_element_by_name("q")
+        # elem.send_keys("Giraffe")
+        # elem.send_keys(Keys.ENTER)
+        # elem = driver.find_element_by_xpath("//*")
+        # source_code= elem.get_attribute('outerHTML')
+        # print(source_code)
+        # driver.close()
 
 main()
 
